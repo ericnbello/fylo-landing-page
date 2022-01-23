80c5278e12f061cea99882ba4a81450f9a355e27
ebello219 <42421358+ebello219@users.noreply.github.com>
2022-01-23 16:34:33 -0500

    updated styling

--- components/common/nav.js
+++ components/common/nav.js
@@ -3,18 +3,18 @@ import Logo from "../images/Logo";
 export default function Nav () {
     return (
         <nav className="flex justify-between p-6 bg-transparent">
-            <div className="flex items-center text-black mr-6 md:pl-20">
+            <div className="flex items-center text-black mr-5 md:pl-20">
                 <Logo />
             </div>
             <div>
                 <ul className="flex px-1 text-sm md:text-lg">
-                    <li className="mr-6 py-6">
+                    <li className="mr-5 py-6">
                         <a className="text-neutral-800 hover:text-[#585989]" href="#!">Features</a>
                     </li>
-                    <li className="mr-6 py-6">
+                    <li className="mr-5 py-6">
                         <a className="text-neutral-800 hover:text-[#585989]" href="#!">Team</a>
                     </li>
-                    <li className="mr-6 py-6">
+                    <li className="mr-5 py-6">
                         <a className="text-neutral-800 hover:text-[#585989]" href="#!">Sign In</a>
                     </li>
                 </ul>
